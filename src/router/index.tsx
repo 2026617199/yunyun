7c007856ad888c85f184608942a6b3fb3e3eee57
zhaojingnan <z2026617199@gmail.com>
2026-03-27 19:03:38 +0800

    接下来我想要 HomePage 不再是根路由，而是一个二级路由，它是一个项目列表页面，然后你重新帮我创建一个HomePage页面，页面的内容暂时先占位

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,15 +1,20 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import CanvasPage from '@/pages/Canvas'
 import HomePage from '@/pages/Home'
+import ProjectListPage from '@/pages/ProjectList'
 
 const router = createBrowserRouter([
     {
-        path: '/canvas/:projectId',
-        element: <CanvasPage />
-    },
-    {
         path: '/',
         element: <HomePage />
+    },
+    {
+        path: '/projects',
+        element: <ProjectListPage />
+    },
+    {
+        path: '/canvas/:projectId',
+        element: <CanvasPage />
     }
 ])
 
